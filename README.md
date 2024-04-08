3f13e60acd44b04c1de30454854723f32dc07393
Mario Rico <mario.rico417@gmail.com>
2024-04-08 17:14:40 -0500

    single file component with useState props

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🎨 Color Picker
+
+Pair Programming with Luke and Mario.
+Site deployed with Netlify
+https://preeminent-panda-f02769.netlify.app/
---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+// Write your Color component here
+import { useState } from "react";
+
+
+const Color = ({color, setSelectedColor}) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
+const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+  return (
+    <>
+    <div id="container">
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className= {selectedColor}>{selectedColor}</div>
+      </div>
+      
+      <div id="colors-list">
+        
+        
+        <Color color="yellow" setSelectedColor={(color) => {setSelectedColor(color)}} />
+        
+        <Color color="green" setSelectedColor={setSelectedColor} />
+
+        <Color color="black" setSelectedColor={setSelectedColor} />
+        
+      </div>
+      
+
+    </div>
+    </>
+  );
+};
+
+export default App;
